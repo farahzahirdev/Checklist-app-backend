58483e68fa9be2a49c8729beb99a7f79edd8698f
farahzahirdev <farahzahir.dev@gmail.com>
2026-04-17 20:28:17 +0500

    feat: add stripe_customer_id to users table and checklist_id to access_windows for post-payment selection

--- alembic/versions/20260417_0001_stripe_cust_id.py
+++ alembic/versions/20260417_0001_stripe_cust_id.py
@@ -1,7 +1,7 @@
 """
 Add stripe_customer_id to users table
 
-Revision ID: 20260417_0001_stripe_customer_id
+Revision ID: 20260417_0001_stripe_cust_id
 Revises: 20260416_0001_rbac_initial
 Create Date: 2026-04-17 00:00:00.000000
 """
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = '20260417_0001_stripe_customer_id'
+revision = '20260417_0001_stripe_cust_id'
 down_revision = '20260416_0001_rbac_initial'
 branch_labels = None
 depends_on = None
--- alembic/versions/20260417_0002_access_window.py
+++ alembic/versions/20260417_0002_access_window.py
@@ -1,8 +1,8 @@
 """
 Add checklist_id to access_windows for post-payment checklist selection
 
-Revision ID: 20260417_0002_access_window_checklist
-Revises: 20260417_0001_stripe_customer_id
+Revision ID: 20260417_0002_access_window
+Revises: 20260417_0001_stripe_cust_id
 Create Date: 2026-04-17 01:00:00.000000
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '20260417_0002_access_window_checklist'
-down_revision = '20260417_0001_stripe_customer_id'
+revision = '20260417_0002_access_window'
+down_revision = '20260417_0001_stripe_cust_id'
 branch_labels = None
 depends_on = None
 
