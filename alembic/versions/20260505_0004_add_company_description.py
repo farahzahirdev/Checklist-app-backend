34f58f02c7fc261b6a26a710d7e6f5cfc23c12f6
farahzahirdev <farahzahir.dev@gmail.com>
2026-05-05 21:48:38 +0500

    feat: Add description column to companies in database schema

---
+++ alembic/versions/20260505_0004_add_company_description.py
@@ -0,0 +1,23 @@
+"""Add description column to companies
+
+Revision ID: 20260505_0004_add_company_description
+Revises: 20260505_0003_add_company_to_payments_access
+Create Date: 2026-05-05 00:00:00
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "20260505_0004_add_company_description"
+down_revision = "20260505_0003_add_company_to_payments_access"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("companies", sa.Column("description", sa.Text(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column("companies", "description")
